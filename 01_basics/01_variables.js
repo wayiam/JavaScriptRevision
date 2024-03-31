74d90c0cc06e61bf770b21bf12904a0438b3db6f
Saksham Singh <60515013+wayiam@users.noreply.github.com>
2024-03-31 09:04:51 +0000

    learning about varibale let, const, var

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 1
+let accountemail = "saksham@google.com"
+var accountemailPass = "12345"
+accountCity = "Delhi"
+let accountState
+
+// accountId = 1;
+
+accountemail = "saks@gmail.com"
+accountemailPass = "23456"
+accountcity="Bengaluru"
+
+/* Prefer not to use var beacause it's issues with block scope and functional scope */
+
+console.table([accountId, accountemail, accountemailPass, accountCity, accountState])
