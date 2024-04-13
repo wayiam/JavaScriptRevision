17e6a36a1636a40fafc230e9ff1be876e10b4aea
Saksham Singh <60515013+wayiam@users.noreply.github.com>
2024-04-13 13:17:54 +0000

    Conversion of Datatypes

---
+++ 01_basics/03_DatatypeConversion.js
@@ -0,0 +1,16 @@
+let value = "NoValue";
+
+let valueNumber = Number(value)
+
+let valueString = String(value)
+
+let valueBoolean = Boolean(value);
+
+console.log("Intial Value: " + value);
+console.log(typeof(value));
+console.log("Using Number : " + valueNumber);
+console.log(typeof(valueNumber));
+console.log("Using String : " + valueString);
+console.log(typeof(valueString));
+console.log("Using Boolean : " + valueBoolean);
+console.log(typeof(valueBoolean));
