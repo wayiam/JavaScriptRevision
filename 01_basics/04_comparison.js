304061e6acb495eae074aa9ff366abdc338d1abb
Saksham Singh <60515013+wayiam@users.noreply.github.com>
2024-04-14 10:58:08 +0000

    Comparison in JavaScript

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,28 @@
+let value = 0;
+
+let stringCompare = "iamValuetoo";
+let booleanValue = true;
+let doubleCompare = 2.99;
+
+console.log(4>2);
+console.log(4>stringCompare);
+console.log(booleanValue>4);
+console.log(doubleCompare>4);
+
+//Comparison with null
+console.log("Below are comparisons with Null and Interger values : ");
+console.log(null > value);
+//here the value will be true because in comparison the value will be converted into datatype(value = 0)
+console.log(null >= value);
+console.log(null == value);
+
+
+//Comparison with null
+console.log("Below are comparisons with Undefined and Interger values : ");
+console.log(undefined > value);
+console.log(undefined >= value);
+console.log(undefined == value);
+
+
+
+
