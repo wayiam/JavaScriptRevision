d6e0fa6a9ee1aeae708a86e354475385dd6a0193
Saksham Singh <60515013+wayiam@users.noreply.github.com>
2024-04-14 11:54:34 +0000

    Math and Number in JavaScript

---
+++ 01_basics/06_MathandNum.js
@@ -0,0 +1,34 @@
+//Number value represents a floating point number like 11 or 12.21
+//There are
+
+const biggestNum = Number.MAX_VALUE;
+const smallestNum = Number.MIN_VALUE;
+const infiniteNum = Number.POSITIVE_INFINITY;
+const negInfiniteNum = Number.NEGATIVE_INFINITY;
+const notANum = Number.NaN;
+
+
+
+console.log(biggestNum);
+console.log(smallestNum);
+console.log(infiniteNum);
+console.log(negInfiniteNum);
+console.log(notANum);
+
+
+// Math
+
+let value = 234.342
+
+console.log(Math.floor(value));
+console.log(Math.ceil(value));
+console.log(Math.abs(value));
+console.log(Math.abs(-value))
+
+
+
+console.log(Math.min(3,7,-2));
+console.log(Math.max(3,7,-2));
+
+
+console.log(Math.floor(Math.random()*10)+1);
